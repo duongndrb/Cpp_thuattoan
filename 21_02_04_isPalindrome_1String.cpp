38fc53c98a3936c0721203a793caebe31c55af67
inhuong3103 <47983821+inhuong3103@users.noreply.github.com>
2021-02-04 09:11:05 +0700

    Create 21_02_04_isPalindrome_1String.cpp

---
+++ 21_02_04_isPalindrome_1String.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+bool isPalindrome(string S)
+{
+    int n = S.size();
+    for(int i = 0; i < n/2; ++i)
+        if(S[i] != S[n - i - 1]) return false;
+    return true;
+}
+
+int main()
+{
+    string my_string;
+    getline(cin, my_string);
+    isPalindrome(my_string);
+    cout << isPalindrome(my_string);
+    return 0;
+}
